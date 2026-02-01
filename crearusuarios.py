b2de39fce9d8e1eba67cf6cc88fcde4710af89bf
Tomas Acosta Romero <141876421+tomasacostar22@users.noreply.github.com>
2026-02-01 15:14:06 -0500

    CREACIÓN DE USUARIOS

--- crearusuarios.py
+++ crearusuarios.py
@@ -1,4 +1,9 @@
-                                                                                                                                                                                   
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+import django
+django.setup()
+
 from usuarios.models import Usuario                                                                                                                                                                               
                                                                                                                                                                                                                     
 usuarios = [                                                                                                                                                                                                      
