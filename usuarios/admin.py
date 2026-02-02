6fd2fab9154337e05a5fed1424eb13dd6a34ed3e
Tomas Acosta Romero <141876421+tomasacostar22@users.noreply.github.com>
2026-02-02 12:06:15 -0500

    cambios admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Usuario
 
-admin.site.register(Usuario, UserAdmin)
+@admin.register(Usuario)
+class UsuarioAdmin(BaseUserAdmin):
+    pass
