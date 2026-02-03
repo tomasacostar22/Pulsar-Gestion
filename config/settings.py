970855e3dcf099953a19d501dedbc2ab28b5615b
Tomas Acosta Romero <141876421+tomasacostar22@users.noreply.github.com>
2026-02-02 19:37:45 -0500

    cambios

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogotá'
 
 USE_I18N = True
 
--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,7 +1,36 @@
+
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
 @admin.register(Usuario)
-class UsuarioAdmin(BaseUserAdmin):
-    pass
+class UsuarioAdmin(UserAdmin):
+    # Campos que se muestran en la lista de usuarios
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    
+    # Campos editables directamente desde la lista (opcional)
+    list_editable = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    
+    # Filtros en la barra lateral
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    
+    # Barra de búsqueda
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    
+    # Organización de campos en el formulario de edición
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Información personal', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permisos', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    # Campos para cuando creas un nuevo usuario
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
